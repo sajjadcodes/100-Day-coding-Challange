a12fd7a98040fcca117ac200db77c2dfbee61726
sajjadcodes <sakraan01@gmail.com>
2022-06-20 15:59:44 +0500

    Day one: print(), input() functions, strings, variable, concatination and commenting

---
+++ Day#01/main.py
@@ -0,0 +1,12 @@
+# Use of print("your content") 
+print("Welcome to the Band name Generator")
+
+# use of variable from user input() function 
+city = input("Enter your city name?\n")
+
+#Enter your pet name
+
+pet = input("Enter your pet name\n")
+
+# connecatination of strings and variable e.g "string" +varable_name
+print("Your Band name is " + city+ " " + pet)
