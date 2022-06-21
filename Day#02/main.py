1cf247a9105f1f28248ad5f6fef1b72604e0489d
sajjadcodes <sakraan01@gmail.com>
2022-06-21 06:12:11 +0500

    day#2 Data Types: String

---
+++ Day#02/main.py
@@ -0,0 +1,19 @@
+# Data Types
+
+#string : anything inside double qoute treated as a string
+
+"Hello"
+# We can also use string character using []
+"Hello"[3]
+# print first character. subscript and subscripting. Start accounting from zero
+"Hello"[0]
+
+# contacatinate : string will not be calculated in this case. 
+
+print("123" + "345")
+
+
+
+
+
+
