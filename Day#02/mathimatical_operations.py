547e65a8fcb9d61da977abaed576d870010ea814
sajjadcodes <sakraan01@gmail.com>
2022-06-21 07:35:44 +0500

    Day#2 challange#2 Find BMI

---
+++ Day#02/mathimatical_operations.py
@@ -0,0 +1,26 @@
+print("Mathematical Operators and operations")
+
+# Addition
+#3 + 5
+
+# Minus
+#7-4
+
+# Multiplication
+#3 * 2
+
+#  Deivision : wil not give the reminder
+#6 / 3
+
+# Power
+#2 ** 2
+
+#  PEMDAS -LR : Parentheses(), Exponents**, (Multiplication*, Division /), (addition + , substraction -) = Equally important. priority left to right
+
+3 *3 +3 / 3 -3
+print(3 *3 +3 / 3 -3)
+
+print("changing priorities")
+print(3 * (3 +3) / 3 -3)
+
+
