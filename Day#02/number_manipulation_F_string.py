ade183f60a33586468b1e9ef558e415f954417d9
sajjadcodes <sakraan01@gmail.com>
2022-06-21 08:17:50 +0500

    number manipluation and usage of f-string

---
+++ Day#02/number_manipulation_F_string.py
@@ -0,0 +1,39 @@
+# float return
+
+print(8/2)
+
+# Avoid floating we can use double //
+
+print(8//2)
+
+#round off using round() function
+
+# It will round off to the close integer
+print(round(2.44444))
+
+# Round of after 2 digit
+print(round(2.4444,2))
+
+#storing number in variable
+
+result  = 6/2
+
+print(result)
+
+#add result with another number
+# shorthand of result = result + 5
+result +=5
+
+print(result)
+
+
+
+#  f string
+
+score = 0
+height = 1.8
+isWinning = True
+
+# f-String: put f"before string{OtherdataType}"
+
+print(f"Your score is {score}, your height is {height}, and winning is {isWinning}")
