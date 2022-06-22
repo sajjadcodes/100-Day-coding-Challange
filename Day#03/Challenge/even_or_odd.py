a8d778a12325081d2f90e9e3aa357c5529be0cef
sajjadcodes <sakraan01@gmail.com>
2022-06-22 09:15:26 +0500

    challenge#01 Even or odd number

---
+++ Day#03/Challenge/even_or_odd.py
@@ -0,0 +1,9 @@
+
+print("Welcome to the even or odd challange")
+
+number = int(input("Which number do you want to check? "))
+
+if number % 2 == 0:
+    print("Your Number is Even")
+else:
+    print("Number is Odd")
