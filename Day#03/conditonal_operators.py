07a24e78d1e87a176504033d8e39d0e5554cbf31
sajjadcodes <sakraan01@gmail.com>
2022-06-22 12:23:31 +0500

    added: logical operators

---
+++ Day#03/conditonal_operators.py
@@ -0,0 +1,23 @@
+print("Welcome to the rellercoaster!")
+height = int(input("What is your height in cm?"))
+bill = 0
+if height >= 120:
+    print("You can ride the rollercoaster!")
+    age = int(input("What is your age?"))
+    if age < 12:
+        bill = 5
+        print("Child tickets are $5")
+    elif age <= 18:
+        bill = 7
+        print("Yout tickets are $7.")
+    elif age >= 45 and age <= 55:
+        print("Everything is going to be ok. Have a ride with us!")
+    else:
+        bill = 12
+        print("Adult tickets are $12")
+    wants_photo = input("Do you want a photo taken? Y or N.")
+    if wants_photo == "y":
+        bill += 3
+    print(f"Your final bill is {bill} ")
+else:
+    print("Sorry, you have to grow taller before you can ride.")
