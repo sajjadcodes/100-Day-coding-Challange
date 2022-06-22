5a48b16684f236931a1d3226f2f3fab77dec7ddc
sajjadcodes <sakraan01@gmail.com>
2022-06-22 12:04:15 +0500

    code-refactory: pizza automation Price

--- Day#03/Challenge/Pizza_automation.py
+++ Day#03/Challenge/Pizza_automation.py
@@ -7,22 +7,18 @@ extra_cheese = input("Do you want extra cheese? Y or N ")
 
 #Write your code below this line 👇
 bill = 0 
-if size == 's':
+if size == 'S':
     bill +=15
-    if add_pepperoni =='y':
+    if add_pepperoni =='Y':
         bill +=2
-    if extra_cheese == 'y':
-        bill +=1
-elif size =='m':
+elif size =='M':
     bill +=20
-    if add_pepperoni == 'y':
+    if add_pepperoni == 'Y':
         bill +=3
-    if extra_cheese == 'y':
-        bill +=1
-elif size == 'l':
+elif size == 'L':
     bill +=25
-    if add_pepperoni == 'y':
+    if add_pepperoni == 'Y':
         bill +=3
-    if extra_cheese == 'y':
-        bill +=1    
+if extra_cheese == 'Y':
+    bill +=1
 print(f"Your final bill is: ${bill}")
