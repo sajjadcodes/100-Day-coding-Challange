b6fc693dbb51317804b643487dfa453625f47350
sajjadcodes <sakraan01@gmail.com>
2022-06-23 10:46:27 +0500

    random float from zero to 5

--- Day#04/random_int.py
+++ Day#04/random_int.py
@@ -5,3 +5,14 @@ random_integer = random.randint(1,20)
 print(random_integer)
 #displying from my_module
 print(my_module.name)
+
+print("Random floating")
+
+rand_float = random.random()
+
+print(rand_float)
+
+# printing float between 0.00 to 5
+my_random = random.random() *5;
+
+print(my_random)
