cc57718de574c7fd04ef61f0f5faf0724925e78e
sajjadcodes <sakraan01@gmail.com>
2022-06-24 12:34:53 +0500

    Find the heighest score in the classroom

---
+++ Day#05/challenge/highest_score_in_the_class.py
@@ -0,0 +1,15 @@
+# 🚨 Don't change the code below 👇
+student_scores = input("Input a list of student scores ").split()
+for n in range(0, len(student_scores)):
+  student_scores[n] = int(student_scores[n])
+print(student_scores)
+# 🚨 Don't change the code above 👆
+
+#Write your code below this row 👇
+heighest = student_scores[0]
+print(heighest)
+for score in student_scores:
+    if heighest < score:
+        heighest = score
+
+print(heighest)
