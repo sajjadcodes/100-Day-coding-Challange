cdbd9e06f0cc332547aac92da4fde0473432b0bf
sajjadcodes <sakraan01@gmail.com>
2022-06-24 12:50:36 +0500

    for loop with range and custom range step

---
+++ Day#05/for_loop_with_custom_steps.py
@@ -0,0 +1,14 @@
+#a to b but not included b
+# for number in range(a,b): default increase number by 1
+# for number in range(a,b,number_of_step): custom increase number 
+#for number in range(a,b):
+	#print(number)
+
+# Will print number from 1 to 20(inclued)
+for number in range(1,21):
+    print(number)
+    
+#custom step
+print("################################################") 
+for number in range(1,21,3):
+    print(number)
