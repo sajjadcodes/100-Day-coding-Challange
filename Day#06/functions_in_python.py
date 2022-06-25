8ea7e6022e31fc65bb7d76ed34fc39664635fd04
sajjadcodes <sakraan01@gmail.com>
2022-06-25 11:51:48 +0500

    Day#06 functions

---
+++ Day#06/functions_in_python.py
@@ -0,0 +1,19 @@
+# name()
+## 
+print("This is also a function")
+print(len("Hello"))
+
+num_char = len("This is length function")
+
+print(num_char)
+
+#writing own functions
+## Create functions
+def name_of_function():
+    print("This is custom function")
+
+
+## execute the function. Trigger the function called calling function
+
+name_of_function()
+    
