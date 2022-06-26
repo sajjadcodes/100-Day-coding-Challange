85603b6573d79a5260bfec5a96209aeb0468e55f
sajjadcodes <sakraan01@gmail.com>
2022-06-27 02:33:22 +0500

    add challenge#03

--- Day#07/Challenge/challeng#02-place-dash_and_reveal_word_if_match.py
+++ Day#07/Challenge/challeng#02-place-dash_and_reveal_word_if_match.py
@@ -11,7 +11,7 @@ print(f'Pssst, the solution is {chosen_word}.')
 #For each letter in the chosen_word, add a "_" to 'display'.
 #So if the chosen_word was "apple", display should be ["_", "_", "_", "_", "_"] with 5 "_" representing each letter to guess.
 
-guess = input("Guess a letter: ").lower()
+
 size = len(chosen_word)
 display = []
 
@@ -25,16 +25,17 @@ print(display)
 #If the letter at that position matches 'guess' then reveal that letter in the display at that position.
 #e.g. If the user guessed "p" and the chosen word was "apple", then display should be ["_", "p", "p", "_", "_"].
 i =0
-for letter in chosen_word:
+while display[i] == "-":
+    guess = input("Guess a letter: ").lower()
+    for letter in chosen_word:
     
-    if letter == guess:
-        print("Right")
-        display[i]= letter
-        i +=1
-    else:
-        print("Wrong")
-        i +=1
+        if letter == guess:
+            display[i]= letter
+            i +=1
+        else:
+            i +=1
+    
 
-
+print(display)
 #TODO-3: - Print 'display' and you should see the guessed letter in the correct position and every other letter replace with "_".
 #Hint - Don't worry about getting the user to guess the next letter. We'll tackle that in step 3.
