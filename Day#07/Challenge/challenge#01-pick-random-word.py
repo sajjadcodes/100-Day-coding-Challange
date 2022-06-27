58d44d15fd8bdd01732eac9ea15e0a02285967ae
sajjadcodes <sakraan01@gmail.com>
2022-06-27 10:46:54 +0500

    day#08

--- Day#07/Challenge/challenge#01-pick-random-word.py
+++ Day#07/Challenge/challenge#01-pick-random-word.py
@@ -7,9 +7,9 @@ word_list = ["aardvark", "baboon", "camel"]
 
 chosen_word = random.choice(word_list)
 
-print(chosen_word)
+
 #TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
-guess = input('Enter your guess letter: ')
+guess = input("Guess a letter: ").lower()
 #TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
 
 for letter in chosen_word:
