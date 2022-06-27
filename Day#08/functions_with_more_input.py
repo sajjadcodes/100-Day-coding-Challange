3444c3b5baab793ca7f8d61d3fc17abd6d6c1137
sajjadcodes <sakraan01@gmail.com>
2022-06-27 11:01:23 +0500

    function with multiple parameters

---
+++ Day#08/functions_with_more_input.py
@@ -0,0 +1,11 @@
+
+# Fuctions with more than 1 inut
+
+def greet_with(name, location):
+    print(f"Hello {name}")
+    print(f"What is it like in {location}")
+
+
+greet_with("Jack Bauer", "Nowhere")
+
+    
