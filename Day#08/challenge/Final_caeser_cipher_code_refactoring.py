14acd4430c6b30d0c6941e5de303255c492ebf94
sajjadcodes <sakraan01@gmail.com>
2022-06-28 13:14:59 +0500

    caeser cipher code refactoring

--- Day#08/challenge/Final_caeser_cipher_code_refactoring.py
+++ Day#08/challenge/Final_caeser_cipher_code_refactoring.py
@@ -7,10 +7,10 @@ shift = int(input("Type the shift number:\n"))
 #TODO-1: Combine the encrypt() and decrypt() functions into a single function called caesar(). 
 def caesar(start_text, shift_amount, cipher_direction):
   end_text = ""
+  if cipher_direction == "decode":
+      shift_amount *= -1
   for letter in start_text:
     position = alphabet.index(letter)
-    if cipher_direction == "decode":
-      shift_amount *= -1
     new_position = position + shift_amount
     end_text += alphabet[new_position]
   print(f"The {cipher_direction}d text is {end_text}")
