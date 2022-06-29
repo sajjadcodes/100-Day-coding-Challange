67bea08ea07a3757c913c96fc7d665fd7c93136a
sajjadcodes <sakraan01@gmail.com>
2022-06-29 13:23:31 +0500

    function_return: convert string into title case format

--- Day#10/functions_with_output.py
+++ Day#10/functions_with_output.py
@@ -1,1 +1,17 @@
 #function with output : return value
+
+def format_name(f_name, l_name):
+    formated_f_name = f_name.title();
+    formated_l_name = l_name.title();
+    
+    return f"{formated_f_name} {formated_l_name}"
+
+
+
+f_name = input("Enter your first name")
+
+l_name = input("Enter your last name")
+
+formated_name = format_name(f_name, l_name)
+
+print(formated_name)
