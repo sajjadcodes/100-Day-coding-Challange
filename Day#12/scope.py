4516982ed8cd849c7c74c1e19c653ab18c5101fa
sajjadcodes <sakraan01@gmail.com>
2022-07-01 12:18:12 +0500

    scope: global variable-access everywhere

--- Day#12/scope.py
+++ Day#12/scope.py
@@ -9,11 +9,20 @@ increase_enemies()
 print(f"enemies outside function: {enemies}")
 
 # Local Scope
-
+# when you create variable inside the function.It is accessible only inside
 def drink_potion():
     potion_strength = 2
     print(potion_strength)
 
 drink_potion()
+# not accessible; error NameError
+#print(potion_strength)
 
-print(potion_strength)
+# Global Scope
+
+play_health  = 10
+
+def drink_potion_1():
+    potion_strength = 2
+    print(play_health)
+drink_potion_1()
