2feb4bac3d682f042f34c55273be9497c0de8c3f
sajjadcodes <sakraan01@gmail.com>
2022-07-01 12:38:33 +0500

    within a function scope

--- Day#12/is_block_level_scope_in_python.py
+++ Day#12/is_block_level_scope_in_python.py
@@ -13,3 +13,14 @@ if game_level < 5:
 
 #all though new_enemy is created inside the if block. but it is accessible outside the block
 print(new_enemy)
+
+# Within function
+def create_enemy():
+    nemeies_list = ["Skeleton","zombie", "Alien"]
+    if game_leve < 3:
+        your_enemy = nemeies_list[0]
+    print(your_enemy)
+    
+
+#will not accessbile outside the function. Because,it is not within a function. So it has scope inside the function
+#print(your_enemy)
