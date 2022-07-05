c129bb1d13ebf33697895dd46e3e1eac7c66adbf
sajjadcodes <sakraan01@gmail.com>
2022-07-05 13:29:16 +0500

    Object oriented attributes and methods

---
+++ Day#16/how_to_use_OOP.py
@@ -0,0 +1,7 @@
+#How to use OOP
+#Waiter has: Attributes / Variable
+##What is has: is_holding_plate = true
+## Tables_responsible = [4,5,6]
+# Does: Methods/functions
+### take_order(table,order)
+### take_payment(amount)
