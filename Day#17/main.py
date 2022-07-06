749ddaf87aec04e46b7c0d40750e24968eac78fd
sajjadcodes <sakraan01@gmail.com>
2022-07-06 13:10:04 +0500

    adding attribute with constructor

--- Day#17/main.py
+++ Day#17/main.py
@@ -1,10 +1,10 @@
 # name PascalCase difference from camelCase , snake_case
 class User:
-    pass
-
-user_1 = User()
-
-print(user_1)
-user_1.id = "1230"
-
+    #initialise attributes 
+    def __init__(self, user_id, username):
+        self.id = user_id
+        self.username = username
+        
+user_1 = User("111","sajjad")
 print(user_1.id)
+print(user_1.username)
