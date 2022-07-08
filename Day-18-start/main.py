f39fdcd924f3c50145df2b683c71e90f9295e419
sajjadcodes <sakraan01@gmail.com>
2022-07-09 04:52:37 +0500

    challenge

--- Day-18-start/main.py
+++ Day-18-start/main.py
@@ -5,10 +5,5 @@ timmy_the_turtle.shape('turtle')
 timmy_the_turtle.color('dark red')
 timmy_the_turtle.forward(100)
 timmy_the_turtle.left(90)
-timmy_the_turtle.forward(100)
-
-
-
-
 screen = Screen()
 screen.exitonclick()
