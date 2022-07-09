5a545e0ff7818cf0c1bb198b8c463d970610da4e
sajjadcodes <sakraan01@gmail.com>
2022-07-09 05:02:42 +0500

    Different way of import

---
+++ Day-18-start/import.py
@@ -0,0 +1,10 @@
+import turtle
+#name = module.name_of_the_class()
+tim = turtle.Turtle()
+
+#from ..import
+#keyword module name keyword thing in module
+from turtle import Turtle
+
+#Import everything
+from turtle import *
