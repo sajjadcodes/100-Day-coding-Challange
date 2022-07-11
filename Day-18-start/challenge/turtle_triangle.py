29dd30d8a23ada02df15aaf636c50276633f0ac1
sajjadcodes <sakraan01@gmail.com>
2022-07-11 23:49:17 +0500

    Day#18 challenge

---
+++ Day-18-start/challenge/turtle_triangle.py
@@ -0,0 +1,13 @@
+from turtle import Turtle, Screen
+
+
+traingle = Turtle()
+
+traingle.right(60)
+traingle.forward(100)
+traingle.right(120)
+traingle.forward(100)
+traingle.right(120)
+traingle.forward(100)
+screen = Screen()
+screen.exitonclick()
