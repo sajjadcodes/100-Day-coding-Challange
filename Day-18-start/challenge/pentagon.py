abc68f2445a778e632fb0033637806fcdc54e387
sajjadcodes <sakraan01@gmail.com>
2022-07-13 02:52:36 +0500

    pentagon

--- Day-18-start/challenge/pentagon.py
+++ Day-18-start/challenge/pentagon.py
@@ -1,9 +1,13 @@
 from turtle import Turtle, Screen
 
 pen = Turtle()
-pen.right(72)
-pen.forward(100)
 
+num_sides = 5
 
+for _ in range(num_sides):
+    angle = 360 / num_sides
+    pen.forward(100)
+    pen.right(angle)
+    
 screen = Screen()
 screen.exitonclick()
